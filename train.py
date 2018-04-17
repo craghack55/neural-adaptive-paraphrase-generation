Correct Epoch Calculation

train.py
@@ -19,9 +19,10 @@ tf.flags.DEFINE_integer('beam_width'         , 5           , '')
 tf.flags.DEFINE_float('learning_rate'       , 0.001         , 'learning rate for the optimizer')
 tf.flags.DEFINE_string('optimizer'          , 'Adam'        , 'Name of the train source file')
 tf.flags.DEFINE_integer('batch_size'        , 100            , 'random seed for training sampling')
-tf.flags.DEFINE_integer('print_every'       , 1           , 'print records every n iteration')
-tf.flags.DEFINE_integer('iterations'        , 10         , 'number of iterations to train')
-tf.flags.DEFINE_integer('block_size'        , 500         , 'number of blocks to train')
+tf.flags.DEFINE_integer('print_every'       , 1000           , 'print records every n iteration')
+tf.flags.DEFINE_integer('iterations'        , 16715         , 'number of iterations to train')
+
+
 tf.flags.DEFINE_string('model_dir'          , 'checkpoints' , 'Directory where to save the model')
 
 tf.flags.DEFINE_integer('input_max_length'  , 30            , 'Max length of input sequence to use')
@@ -30,16 +31,33 @@ tf.flags.DEFINE_integer('output_max_length' , 30            , 'Max length of out
 tf.flags.DEFINE_bool('use_residual_lstm'    , True          , 'To use the residual connection with the residual LSTM')
 
 # Data related
-tf.flags.DEFINE_string('input_filename', 'data/mscoco/train_source.txt', 'Name of the train source file')
+tf.flags.DEFINE_string('input_filename', 'data/mscoco/target1.txt', 'Name of the train source file')
 tf.flags.DEFINE_string('output_filename', 'data/mscoco/train_target.txt', 'Name of the train target file')
 tf.flags.DEFINE_string('input_test_filename', 'data/mscoco/test_source.txt', 'Name of the train source file')
 tf.flags.DEFINE_string('output_test_filename', 'data/mscoco/test_target.txt', 'Name of the train target file')
 tf.flags.DEFINE_string('vocab_filename', 'data/mscoco/train_vocab.txt', 'Name of the vocab file')
 
+
+# MSCOCO - 167149 data points.
+
 def evaluate(reference_corpus, translation_corpus):
     bleu = bleu_hook.compute_bleu(reference_corpus, translation_corpus)
     return bleu
 
+# Restore from checkpoint. Resume training with different dataset.
+def trainWithPreviousKnowledge():
+	print("")
+
+# Keep checkpoint folders seperate. Restart training with each different block.
+def trainWithoutPreviousKnowledge():
+	print("")
+
+# Train with Quora. Restore from the checkpoint. Start training with MSCOCO.
+def trainWithTransferLearning(sourceDataset, transferMethod):
+	print("")
+
+def trainWithActiveLearning(samplingMethod):
+	print("")
 
 def main(argv):
     tf.logging._logger.setLevel(logging.INFO)
@@ -53,7 +71,7 @@ def main(argv):
             formatter=data.get_formatter(['source', 'target', 'predict']))
 
     estimator = tf.estimator.Estimator(model_fn=model.make_graph, model_dir=FLAGS.model_dir, params=FLAGS)
-    estimator.train(input_fn=input_fn, hooks=[tf.train.FeedFnHook(feed_fn), print_inputs], steps=None)
+    estimator.train(input_fn=input_fn, hooks=[tf.train.FeedFnHook(feed_fn), print_inputs], steps=FLAGS.iterations)
 
     test_paraphrases = list(estimator.predict(test_fn))
     data.builtTranslationCorpus(test_paraphrases)